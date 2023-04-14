8ec1390ee39c1fbd917bf53007c6276f0de81297
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2023-04-14 16:03:17 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo_new.gif';
-import './App.css';
+
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+
+import Home from "./component/Home/Home";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My First React Project.    
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+         <Routes>
+           <Route path="/" element ={<Home/>} />
+           <Route path="/about" element ={<h1>hello about</h1>} />
+         </Routes>
+       </Router>
     </div>
   );
 }
