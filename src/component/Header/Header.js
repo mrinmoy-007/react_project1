03da6b9c3c65ff66b7b3490015a0412b7b4e8fc2
sardar.arup19967@gmail.com <sardar.arup19967@gmail.com>
2023-04-22 13:21:16 +0530

    logosection add2

---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+import ToggleIcom from "../../Image/toggle-icon.png"
+
+export default function Header(){
+    return(
+        <> 
+        
+        </>
+    );
+}
